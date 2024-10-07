a4bd99c60621cbbfc018213c9173547e61659e49
Rafael Sachetto <rafael@sachetto.com.br>
2024-10-07 18:25:36 -0300

    add sf mono font

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ base=(
   "font-fira-code-nerd-font --cask"
   "font-sf-pro --cask"
   "font-sf-compact --cask"
+  "font-sf-mono --cask"
   "font-new-york --cask"
   "git; rm ~/.gitconfig ~/.gitignore; ln -s $(pwd)/.gitconfig ~/.gitconfig; ln -s $(pwd)/.gitignore ~/.gitignore"
   "docker --cask"
